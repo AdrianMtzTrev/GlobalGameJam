d90936be524462c7c2e9d8c9baa1653fdcbafbc4
JesusLe08 <jisusleal15@gmail.com>
2026-02-01 07:56:36 -0600

    Spawnpoint Boss1

--- UI/Scripts/Escena2_Fade1.gd
+++ UI/Scripts/Escena2_Fade1.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+@onready var player = $Player
+
 func _ready():
+	if Game.spawn_point_name != "":
+		var marker = get_node_or_null(Game.spawn_point_name)
+		if marker:
+			player.global_position = marker.global_position
+			Game.spawn_point_name = ""
 	await Fade.fade_from_black(1.0)
