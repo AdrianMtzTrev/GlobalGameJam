060ae41babdd8cf0e83677adcf6e96817d2f63a9
JesusLe08 <jisusleal15@gmail.com>
2026-02-01 05:07:44 -0600

    Transicion a Boss1

---
+++ Scenes/CambioScene_Boss1.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var next_scene_path: String
+
+func _on_body_entered(body: Node2D) -> void:
+	if  body.name == "Player":
+		body.set_physics_process(false) # opcional
+		await Fade.fade_to_black(0.25)
+		await get_tree().create_timer(1.0).timeout
+		change_scene()
+		
+func change_scene():
+	get_tree().change_scene_to_file(next_scene_path)
